872b9c3ee805f10dc3b55d820d688ac4eaa4e9ec
Blake Frederick <bf@six12creative.com>
2022-11-05 11:37:57 -0700

    Make prettier with prettier

--- app/page.jsx
+++ app/page.jsx
@@ -5,25 +5,23 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome!
-        </h1>
+        <h1 className={styles.title}>Welcome!</h1>
 
         <div className="text-xl m-10 font-bold">
           Take a look at this fake skeleton loader pulsing infinitely
         </div>
 
-        <div class="border border-blue-300 shadow rounded-md p-4 m-4 max-w-sm w-full mx-auto">
-          <div class="animate-pulse flex space-x-4">
-            <div class="rounded-full bg-slate-400 h-10 w-10"></div>
-            <div class="flex-1 space-y-6 py-1">
-              <div class="h-2 bg-slate-500 rounded"></div>
-              <div class="space-y-3">
-                <div class="grid grid-cols-3 gap-4">
-                  <div class="h-2 bg-slate-400 rounded col-span-2"></div>
-                  <div class="h-2 bg-slate-400 rounded col-span-1"></div>
+        <div className="border border-blue-300 shadow rounded-md p-4 m-4 max-w-sm w-full mx-auto">
+          <div className="animate-pulse flex space-x-4">
+            <div className="rounded-full bg-slate-400 h-10 w-10"></div>
+            <div className="flex-1 space-y-6 py-1">
+              <div className="h-2 bg-slate-500 rounded"></div>
+              <div className="space-y-3">
+                <div className="grid grid-cols-3 gap-4">
+                  <div className="h-2 bg-slate-400 rounded col-span-2"></div>
+                  <div className="h-2 bg-slate-400 rounded col-span-1"></div>
                 </div>
-                <div class="h-2 bg-slate-400 rounded"></div>
+                <div className="h-2 bg-slate-400 rounded"></div>
               </div>
             </div>
           </div>
@@ -33,25 +31,24 @@ export default function Home() {
           Now bouncing infinitely
         </div>
 
-        <div class="border border-green-300 shadow rounded-md p-4 m-4 max-w-sm w-full mx-auto animate-bounce">
-          <div class="flex space-x-4">
-            <div class="rounded-full bg-slate-200 h-10 w-10"></div>
-            <div class="flex-1 space-y-6 py-1">
-              <div class="h-2 bg-slate-200 rounded"></div>
-              <div class="space-y-3">
-                <div class="grid grid-cols-3 gap-4">
-                  <div class="h-2 bg-slate-200 rounded col-span-2"></div>
-                  <div class="h-2 bg-slate-200 rounded col-span-1"></div>
+        <div className="border border-green-300 shadow rounded-md p-4 m-4 max-w-sm w-full mx-auto animate-bounce">
+          <div className="flex space-x-4">
+            <div className="rounded-full bg-slate-200 h-10 w-10"></div>
+            <div className="flex-1 space-y-6 py-1">
+              <div className="h-2 bg-slate-200 rounded"></div>
+              <div className="space-y-3">
+                <div className="grid grid-cols-3 gap-4">
+                  <div className="h-2 bg-slate-200 rounded col-span-2"></div>
+                  <div className="h-2 bg-slate-200 rounded col-span-1"></div>
                 </div>
-                <div class="h-2 bg-slate-200 rounded"></div>
+                <div className="h-2 bg-slate-200 rounded"></div>
               </div>
             </div>
           </div>
         </div>
       </main>
 
-      <footer className={styles.footer}>
-      </footer>
+      <footer className={styles.footer}></footer>
     </div>
   )
 }
