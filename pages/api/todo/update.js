035542634c4b384bd1129f7cd004c2c7f16f3cd8
Blake Frederick <bf@six12creative.com>
2022-11-09 21:13:42 -0800

    Try to fix update on live by awaiting

--- pages/api/todo/update.js
+++ pages/api/todo/update.js
@@ -1,9 +1,10 @@
 import { updateTodo } from './list'
 
-export default function handler(req, res) {
+export default async function handler(req, res) {
   if (req.method === 'POST') {
     let { id, name, isDone } = JSON.parse(req.body)
-    updateTodo({ id, isDone: Boolean(isDone) })
+    const updateResult = await updateTodo({ id, isDone: Boolean(isDone) })
+    console.log(updateResult)
     res.status(200).json({ msg: 'todo updated' })
   } else {
     res.status(400).json({ msg: 'invalid request method' })
